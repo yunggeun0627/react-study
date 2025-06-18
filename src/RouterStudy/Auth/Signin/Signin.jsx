e604349614e480772f1d7b7348b8789743a8f527
yunggeun0627 <bbiyagi465@gmail.com>
2025-06-18 17:37:17 +0900

    modified:   src/RouterStudy/Auth/Signin/Signin.jsx

--- src/RouterStudy/Auth/Signin/Signin.jsx
+++ src/RouterStudy/Auth/Signin/Signin.jsx
@@ -158,7 +158,11 @@ function Signin(props) {
     }, [inputItems]);
 
     const handleRegisterOnClick = async () => {
-        const url = "http://localhost:8080/api/users";
+        const url = "http://localhost:8080/api/users/login";
+
+        // 컨트롤러 메소드명 login
+        // Dto명 LoginDto
+        // POST요청
 
         let data = {};
 
@@ -171,9 +175,9 @@ function Signin(props) {
 
         try {
             await axios.post(url, data);
-            alert("사용자 등록 완료");
+            alert("로그인 요청 완료");
         } catch(error) {
-            alert("사용자 등록 오류");
+            alert("로그인 오류");
         }
         
     }
